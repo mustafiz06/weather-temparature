8737e4c91f357c991c82b83635f68aeaf042f830
mustafiz06 <themustafiz06@gmail.com>
2023-06-15 03:33:42 +0600

    search field clear code added

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 const API_KEY = `317f28873c0c20bb6c0fc598017e2cbd`;
 
 const loadTemparature = city => {
-    const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
     fetch(url)
         .then(res => res.json())
         .then(data => displayTemparature(data))
@@ -14,12 +14,13 @@ const displayTemparature = data => {
 //     setInnerTextById('temparature', data.main.temp);
 //     setInnerTextById('condition', data.weather[0].main)
 // }
-document.getElementById('search-btn').addEventListener('click',function () {
+document.getElementById('search-btn').addEventListener('click', function () {
     const searchField = document.getElementById('search_field');
     const city = searchField.value;
     loadTemparature(city)
-    const displayCity=document.getElementById('display-city');
-    displayCity.innerText=city
+    const displayCity = document.getElementById('display-city');
+    displayCity.innerText = city;
+    searchField.value = ''
 })
 
 
